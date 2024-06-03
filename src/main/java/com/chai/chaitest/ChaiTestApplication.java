acc37887064733764fa4db093bfb2708afbc00c9
chai <chaiminghui@meione.cc>
2024-06-03 11:10:42 +0800

    Initial commit

---
+++ src/main/java/com/chai/chaitest/ChaiTestApplication.java
@@ -0,0 +1,13 @@
+package com.chai.chaitest;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ChaiTestApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ChaiTestApplication.class, args);
+	}
+
+}
